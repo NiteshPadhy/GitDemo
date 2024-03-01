a1d856a7240d6e5572727e5959409f2df64ed41a
nitesh <niteshpadhy0007@gmail.com>
2024-03-01 17:39:45 +0530

    added data

--- src/main/java/com/code/model/Empoloyee.java
+++ src/main/java/com/code/model/Empoloyee.java
@@ -1,13 +1,16 @@
 package com.code.model;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
 public class Empoloyee {
 	
 	private String id;
 	private String name;
+	private String email;
 
 }
